e66913810de79a4d1f04856c5612ff0693586058
sungkyu <kskyu610@gmail.com>
2020-04-06 15:17:54 +0900

    add register function

---
+++ src/main/java/com/study/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.study.springboot.service.posts;
+
+
+import com.study.springboot.domain.posts.PostsRepository;
+import com.study.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
