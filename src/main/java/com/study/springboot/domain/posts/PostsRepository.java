f1095a1349f1c80adfb85938dc5a42ea058acc9d
sungkyu <kskyu610@gmail.com>
2020-05-03 17:19:25 +0900

    add register article function

--- src/main/java/com/study/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/study/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.study.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")  //SpringDataJpa에서 제공하는 기본 메소드로 해결 가능
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/springboot/service/posts/PostsService.java
@@ -3,13 +3,16 @@ package com.study.springboot.service.posts;
 
 import com.study.springboot.domain.posts.Posts;
 import com.study.springboot.domain.posts.PostsRepository;
+import com.study.springboot.web.dto.PostsListResponseDto;
 import com.study.springboot.web.dto.PostsResponseDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +29,12 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 개시글이 없습니다. id = "+ id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id){
